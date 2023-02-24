afdd4150574c7701ace405bbeb65ff68e6f36d1b
rayyks <rayydna14@gmail.com>
2023-02-24 19:40:15 +0700

    adding assets DLL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Components
 import { Dashboard } from "./pages/Dashboard/Dashboard.jsx";
 import { About } from "./pages/About/About.jsx";
+import { Project } from "./pages/Projects/Project.jsx";
+import { Contact } from "./pages/Contact/Contact.jsx";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Projects/Project.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Project() {
+  return (
+    <div className="Project">
+      <div className="Container">MY PROJECT</div>
+    </div>
+  );
+}
