72890ff09e1bccfc3c7edb9ffdc30287aa102219
rayyks <rayydna14@gmail.com>
2023-02-25 12:18:28 +0700

    update navbar

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-// import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <nav className="Navbar ">
-      <div className="container bg-black">
-        <ul>
-          <li className="nav-link">Home</li>
-          <li className="nav-link">About</li>
-          <li className="nav-link">Project</li>
-          <li className="nav-link btn">Contact</li>
-        </ul>
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+      <div class="container flex flex-wrap items-center justify-between mx-auto">
+        <div>
+          <NavLink to="/" className="nav-link text-white">
+            Home
+          </NavLink>
+          <NavLink to="/about" className="nav-link text-white">
+            About
+          </NavLink>
+          <NavLink to="/project" className="nav-link text-white">
+            Project
+          </NavLink>
+          <NavLink to="/contact" className="nav-link text-white">
+            Contact
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
--- src/pages/Navbar/SharedLayout.jsx
+++ src/pages/Navbar/SharedLayout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-// import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
 
 export function SharedLayout() {
   return (
     <>
       <Navbar />
-      {/* <Outlet /> */}
+      <Outlet />
     </>
   );
 }
